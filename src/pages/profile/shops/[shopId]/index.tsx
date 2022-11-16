8069baf9797e732125cf6c3f22ba28426b4b500f
Theinmwe Naing <theinmwenaing22@gmail.com>
2022-11-16 21:10:27 +0630

    update shop overview

--- src/pages/profile/shops/[shopId]/index.tsx
+++ src/pages/profile/shops/[shopId]/index.tsx
@@ -1,51 +1,14 @@
-import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 import Link from "next/link";
-import { formatPrice, formatTimestamp } from "../../../../common/utils";
-import { Input } from "../../../../components/forms";
+import Dropdown from "../../../../components/Dropdown";
 import Pagination from "../../../../components/Pagination";
-
-function ProductRow() {
-  return (
-    <tr>
-      <td className="ps-3 ps-lg-4" style={{ minWidth: 300 }}>
-        <span>
-          Product Name Here Product Name Here Product Name Here Product Name
-          Here Product Name Here
-        </span>
-      </td>
-      <td>
-        <span className="text-nowrap me-3">Category</span>
-      </td>
-      <td>
-        <span className="text-nowrap me-3">{formatPrice(50000)} Ks</span>
-      </td>
-      <td>
-        <span className="text-nowrap me-3">
-          {formatTimestamp(new Date().getTime())}
-        </span>
-      </td>
-      <td>
-        <div className="hstack align-items-center gap-2 me-3">
-          <Link href={`/profile/shops/id/id`}>
-            <a className="btn btn-primary">
-              <PencilSquareIcon width={20} />
-            </a>
-          </Link>
-          <button
-            disabled={false}
-            className="btn btn-danger"
-            onClick={async () => {}}
-          >
-            <TrashIcon width={20} />
-          </button>
-        </div>
-      </td>
-    </tr>
-  );
-}
+import ProductManageGridItem from "../../../../components/product/ProductManageGridItem";
 
 function ShopDetail() {
   const list = [1, 2, 3, 4, 5];
+  let image = `https://source.unsplash.com/random/200x240?random=${Math.floor(
+    Math.random() * 100
+  )}`;
 
   return (
     <div className="vstack">
@@ -77,77 +40,97 @@ function ShopDetail() {
         </div>
       </div>
       <div className="container py-4">
-        <div className="card-group mt-2">
-          <div className="card bg-white">
-            <div className="p-3">
-              <h4 className="title">138</h4>
-              <span>Total Products</span>
-            </div>
-          </div>
-          <div className="card bg-white">
-            <div className="p-3">
-              <h4 className="title">30</h4>
-              <span>Orders</span>
-            </div>
-          </div>
-          <div className="card bg-white">
-            <div className="p-3">
-              <h4 className="title">12</h4>
-              <span>Awaiting delivery</span>
-            </div>
-          </div>
-          <div className="card bg-white">
-            <div className="p-3">
-              <h4 className="title">50</h4>
-              <span>Delivered items</span>
+        <div className="row">
+          <div className="col-12">
+            <div className="border rounded bg-white vstack overflow-hidden">
+              <div
+                style={{
+                  width: "100%",
+                  height: 200,
+                }}
+                className="position-relative"
+              >
+                <Image
+                  src={image}
+                  alt=""
+                  layout="fill"
+                  objectFit="cover"
+                  priority
+                />
+              </div>
+              <div className="row p-3 py-sm-4">
+                <div className="col">
+                  <div className="hstack">
+                    <div className="flex-shrink-0 mt-n9">
+                      <Image
+                        src={image}
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="border rounded border-white border-4"
+                        objectFit="cover"
+                      />
+                    </div>
+                    <div className="ms-2 d-flex flex-column mt-n2 mt-sm-n3">
+                      <h4 className="mb-0">{"Shoes World"}</h4>
+                      <div className="text-muted small mb-1 text-truncate">
+                        {"Shop HeadLine"}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-auto">
+                  <div
+                    className="mt-2 mt-sm-0 gap-1 hstack"
+                    style={{ zIndex: 999 }}
+                  >
+                    <div className="flex-grow-1 d-none d-md-block"></div>
+                    <Dropdown
+                      toggle={"Shop Action"}
+                      toggleClassName="dropdown-toggle"
+                    >
+                      <li role="button" className="dropdown-item">
+                        Edit
+                      </li>
+                      <li role="button" className="dropdown-item">
+                        Delete
+                      </li>
+                    </Dropdown>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className="card mt-3 mb-3">
           <div className="card-header bg-white fs-4 fw-semibold p-3">
-            Products
+            <div className="hstack">
+              Products
+              <div className="ms-auto">
+                <Link href="/profile/shops/1/create-product">
+                  <a className="btn btn-primary h-100 hstack">Create new</a>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="card-body p-0">
+          <div className="card-body">
             <div className="vstack">
-              <div className="p-3 p-lg-4">
-                <div className="row g-3">
-                  <div className="col">
-                    <Input
-                      id="filterProductsInput"
-                      name="filter"
-                      type="text"
-                      placeholder="Search your products"
-                    />
+              <div className="card-body">
+                <div className="vstack">
+                  <div className="row g-3">
+                    {list.map((i) => {
+                      return (
+                        <div className="col-lg-3" key={i}>
+                          <ProductManageGridItem />
+                        </div>
+                      );
+                    })}
                   </div>
-                  <div className="col-auto">
-                    <Link href="/profile/shops/1/create-product">
-                      <a className="btn btn-primary h-100 hstack">Create new</a>
-                    </Link>
+
+                  <div className="d-flex justify-content-end pt-3 px-3">
+                    <Pagination hasPrev={true} hasNext={true} />
                   </div>
                 </div>
-              </div>
-
-              <div className="table-responsive">
-                <table className="table bg-white align-middle">
-                  <thead className="table-light text-nowrap align-middle">
-                    <tr style={{ height: 50 }}>
-                      <th className="ps-3 ps-lg-4 fw-medium">NAME</th>
-                      <th className="fw-medium">CATEGORY</th>
-                      <th className="fw-medium">PRICE</th>
-                      <th className="fw-medium">CREATED AT</th>
-                      <th className="fw-medium"></th>
-                    </tr>
-                  </thead>
-                  <tbody className="border-top-0">
-                    {list.map((i) => (
-                      <ProductRow key={i} />
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="d-flex justify-content-end pt-3 px-3">
-                <Pagination hasPrev={true} hasNext={true} />
               </div>
             </div>
           </div>
